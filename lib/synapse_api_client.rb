ad70cce85b7cae624bad0e604638f8eadfab3094
Adam Saegebarth <adams@synapse.com>
2012-09-26 14:04:54 -0700

    add api key functionality to client

--- lib/synapse_api_client.rb
+++ lib/synapse_api_client.rb
@@ -14,12 +14,19 @@ module SynapseApiClient
          conn.response :logger
          conn.response :raise_error
          conn.use :instrumentation
-
          conn.adapter :net_http
          conn.headers[:user_agent] = 'MyLib v1.2'
        end
     end
     attr_writer :connection
+
+    def api_key
+      defined? @api_key and @api_key or raise(
+        ConfigurationError, "Recurly.api_key not configured"
+      )
+    end
+
+    attr_writer :api_key
 
   end
 end
--- lib/synapse_api_client/wifi_password.rb
+++ lib/synapse_api_client/wifi_password.rb
@@ -4,6 +4,7 @@ module SynapseApiClient
       @connection = SynapseApiClient.connect
       response = @connection.get do |req|
         req.url "wifi_password.json"
+        req.params['auth_token'] = SynapseApiClient.api_key
       end
       response.body
     end
