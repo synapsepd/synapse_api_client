d34721eb115ec8af5ab439b7ddb6e4da9a6e6db5
Adam Saegebarth <adams@synapse.com>
2012-10-20 22:52:56 -0700

    Add filters to user class

--- lib/synapse_api_client/user.rb
+++ lib/synapse_api_client/user.rb
@@ -9,12 +9,16 @@ module SynapseApiClient
       response.body
     end
 
-    def self.search(query)
+    def self.search(query=nil)
       @connection = SynapseApiClient.connect
       response = @connection.get do |req|
-        req.url "users/search.json"
         req.params['auth_token'] = SynapseApiClient.api_key
-        req.params['q'] = query
+        if query
+          req.url "users/search.json"
+          req.params['q'] = query
+        else
+          req.url "users.json"
+        end
       end
       response.body
     end
@@ -46,8 +50,27 @@ module SynapseApiClient
       response.body
     end
 
-    def where(field_name, value)
-      @users = @users.find_all { |person| person.field_name == params[:value] }
+    def filter_by(field_names = [])
+      field_names.each do |f|
+        @users = @users.find_all { |u| u.field_name == params[field_name.to_sym]}
+      end
+      @users
+    end
+
+    def sort_by(field_name, order)
+      if field_name
+        if field_name == 'name'
+          @users = @users.sort! { |a, b| a.last_name <=> b.last_name }
+        elsif field_name == 'start_date'
+          @users = @users.sort! { |a, b| a.start_date <=> b.start_date }
+        elsif field_name == 'department'
+          @users = @users.sort! { |a, b| a.department <=> b.department }
+        end
+
+        if order == 'desc'
+          @users.reverse
+        end
+      end
     end
   end
 end
